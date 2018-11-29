5c3c00ac41844c584d1d48c038672db2aa605024
Lewis Mojica <mojicalewis@gmail.com>
2018-11-29 14:26:06 -0500

    Se implementan funciones en el Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "A4988.h"
 #include <definitions/definitions.h>
+#include "functions/Conveyor/Conveyor.h"
+#include "functions/PickAndPlace/PickAndPlace.h"
 
 //conveyor
 A4988 motor_conveyor(conveyor_steps,conveyor_pin_DIR,conveyor_pin_STEP);
@@ -11,13 +13,25 @@ A4988 motor_X_pick(pick_and_place_X_steps,pick_and_place_X_pin_DIR,pick_and_plac
 //pick and place
 A4988 motor_Y_pick(pick_and_place_Y_steps,pick_and_place_Y_pin_DIR,pick_and_place_Y_pin_STEP);
 
+//stores the pick up position
+struct pick_up_pos{
+  unsigned long x;
+  unsigned long y;
+  };
+
 void setup() {
   motor_conveyor.begin();
   motor_X_pick.begin();
   motor_Y_pick.begin();
-  
+  goToHome();
+
+
 }
 
 void loop() {
-  
+  nextItem();
+
+  if(isLampInPosition()){
+    discardLamp(lampState());
+  }
 }
