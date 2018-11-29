5ae63b08fbcffa0e348d7ddef9b402253c05526c
Lewis Mojica <mojicalewis@gmail.com>
2018-11-29 09:38:06 -0500

    Se crean definiciones de motores paso a paso

---
+++ src/definitions/definitions.h
@@ -0,0 +1,15 @@
+//////// STEPPER MOTORS CONFIG ////////
+    // CONVEYOR
+        #define conveyor_steps 200
+        #define conveyor_pin_DIR 8
+        #define conveyor_pin_STEP 9
+
+    // PICK AND PLACE
+        // X AXIS
+        #define pick_and_place_X_steps 200
+        #define pick_and_place_X_pin_DIR 10
+        #define pick_and_place_X_pin_STEP 11
+        // Y AXIS
+        #define pick_and_place_Y_steps 200
+        #define pick_and_place_Y_pin_DIR 12
+        #define pick_and_place_Y_pin_STEP 13
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include "A4988.h"
+#include <definitions/definitions.h>
+
+//conveyor
+A4988 motor_conveyor(conveyor_steps,conveyor_pin_DIR,conveyor_pin_STEP);
+
+//pick and place
+A4988 motor_X_pick(pick_and_place_X_steps,pick_and_place_X_pin_DIR,pick_and_place_X_pin_STEP);
+
+//pick and place
+A4988 motor_Y_pick(pick_and_place_Y_steps,pick_and_place_Y_pin_DIR,pick_and_place_Y_pin_STEP);
 
 void setup() {
-  // put your setup code here, to run once:
+  motor_conveyor.begin();
+  motor_X_pick.begin();
+  motor_Y_pick.begin();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
