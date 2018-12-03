00c607d7cba8c825068e7a22009672c3acda4ca9
Lewis Mojica <mojicalewis@gmail.com>
2018-12-02 21:24:23 -0500

    La funcion goToHome calibra el eje x el pick and place

--- src/functions/PickAndPlace/PickAndPlace.cpp
+++ src/functions/PickAndPlace/PickAndPlace.cpp
@@ -6,19 +6,18 @@
 extern CartesianPos pick_up_pos;
 extern A4988 motor_X_pick;
 extern A4988 motor_Y_pick;
+extern bool stopit;
 
 
 void goToHome(){
-    byte exit = 0;
-    while(exit < 2){
-        if(digitalRead(limit_switch_x)){
-            motor_X_pick.rotate(5);
-        }else exit += 1;
-        if(digitalRead(limit_switch_y)){
-            motor_Y_pick.rotate(5);
-        }else exit += 1;
-    }
-    pick_up_pos.setHome();
+
+    /* PRIMERO VA EL EJE Y*/
+    motor_X_pick.setRPM(100);
+    digitalWrite(pick_and_place_X_pin_DIR,HIGH);
+    motor_X_pick.move(3100);
+    pinMode(9,OUTPUT);
+    digitalWrite(9,LOW);
+
 }
 
 void pushDown(){
