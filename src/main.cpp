7e9334a709e44fb38879440c7a4e671b8503155a
Lewis Mojica <mojicalewis@gmail.com>
2018-12-04 00:48:32 -0500

    adding some functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,12 @@
 #include <definitions/definitions.h>
 #include "functions/Conveyor/Conveyor.h"
 #include "functions/PickAndPlace/PickAndPlace.h"
+#include "Display/Display.h"
 #define MOTOR_ACCEL 4000
 #define MOTOR_DECEL 1000
 // #include <Wire.h> 
 // #include <LiquidCrystal_I2C.h>
 
-bool go;
-bool stopit = false;
 // LiquidCrystal_I2C lcd(0x3F,20,4);
 //conveyor
 A4988 motor_conveyor(conveyor_steps,9,conveyor_pin_STEP);
@@ -21,22 +20,17 @@ A4988 motor_X_pick(pick_and_place_X_steps  ,  pick_and_place_X_pin_DIR ,  pick_a
 //pick and place
 A4988 motor_Y_pick(pick_and_place_Y_steps  ,  pick_and_place_Y_pin_DIR  ,  pick_and_place_Y_pin_STEP , pick_and_place_X_pin_ENABLE);
 
-CartesianPos pick_up_pos;
-
-void stop();
 
 void setup() {
+  welcomeScreen();
   pinMode(limit_switch_x, INPUT_PULLUP);
   motor_X_pick.begin(180);
-  digitalWrite(13,LOW);
   Serial.begin(19200);
-  // goToHome();
-
-  // lampState();
-motor_X_pick.move(-500);
+  goToHome();
 }
 
 void loop() {
+  static bool go;
 if(Serial.available() > 0){
   Serial.read();
   go = !go;
