f2a84253df53a077bbbbb3a18d282249a7d10117
Lewis Mojica <mojicalewis@gmail.com>
2018-12-07 17:17:21 -0500

    nuevas funciones del display

--- src/functions/Conveyor/Display/Display.cpp
+++ src/functions/Conveyor/Display/Display.cpp
@@ -1,12 +1,28 @@
 #include <Display/Display.h>
 
 void welcomeScreen(){
+    lcd.clear();
     lcd.init();                      // initialize the lcd   
     lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.print(" Sistema de control");
+    lcd.setCursor(0,1);
+    lcd.print("   de calidad de");
+    lcd.setCursor(0,2);
+    lcd.print("   bombillas LED");
+    delay(5000);
+    lcd.clear();
+
     lcd.setCursor(0,1);
     lcd.print("   Calibrando...");  
+}
+
+void standByScreen(){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("  PRESIONE BOTON DE");  
+    lcd.setCursor(0,1);
+    lcd.print("INICIO PARA COMENZAR");
     lcd.setCursor(0,2);
-    // lcd.print("www.amazon.com/shops");
-    lcd.setCursor(0,3);
-    // lcd.print("/smraza");
+    lcd.print("  PROCESO DE PRUEBA");
 }
--- src/functions/Conveyor/Display/Display.h
+++ src/functions/Conveyor/Display/Display.h
@@ -2,4 +2,5 @@
 #include <LiquidCrystal_I2C.h>
 
 void welcomeScreen();
+void standByScreen();
 extern LiquidCrystal_I2C lcd;
