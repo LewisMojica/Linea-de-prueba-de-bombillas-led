d03ce5eff078e729d7041ea10fb500be8a63c1f4
Lewis Mojica <mojicalewis@gmail.com>
2018-12-06 22:27:24 -0500

    Funcino de bienvenidad habilitada

--- src/functions/Conveyor/Display/Display.cpp
+++ src/functions/Conveyor/Display/Display.cpp
@@ -1,5 +1,12 @@
 #include <Display/Display.h>
 
 void welcomeScreen(){
-
+    lcd.init();                      // initialize the lcd   
+    lcd.backlight();
+    lcd.setCursor(0,1);
+    lcd.print("   Calibrando...");  
+    lcd.setCursor(0,2);
+    // lcd.print("www.amazon.com/shops");
+    lcd.setCursor(0,3);
+    // lcd.print("/smraza");
 }
--- src/functions/Conveyor/Display/Display.h
+++ src/functions/Conveyor/Display/Display.h
@@ -1,3 +1,5 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
 void welcomeScreen();
+extern LiquidCrystal_I2C lcd;
